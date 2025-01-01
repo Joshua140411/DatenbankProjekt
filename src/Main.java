d8b6b73be4971e3549f00e66392019db658fe239
Joshua Sawadda <joshua.sawadda001@stud.fh-dortmund.de>
2025-01-01 16:56:33 +0100

    SQL ANFRAGE 1 Grundgerüst

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import Backend.NpcService;
 import Backend.RuestungService;
 import Backend.RuestungsTypService;
+import Daos.NpcDao;
 import Daos.RuestungDao;
 import Daos.RuestungsTypDao;
 import Frontend.Menu;
@@ -7,7 +9,7 @@ import Frontend.Menu;
 public class Main {
 
     public static void main(String[] args) {
-        Menu menu = new Menu(new RuestungService(new RuestungDao()), new RuestungsTypService(new RuestungsTypDao()));
+        Menu menu = new Menu(new RuestungService(new RuestungDao()), new RuestungsTypService(new RuestungsTypDao()), new NpcService(new NpcDao()));
         menu.createMenu();
     }
 }
